6915746ba8d5eb90d316f8da70a0d331ad2307b6
Igor Pertsav <pertsavigorstray@gmail.com>
2012-03-03 17:56:23 +0300

    Title_change

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,17 +3,43 @@ require 'spec_helper'
 describe PagesController do
 
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'home'
       response.should be_success
     end
   end
 
   describe "GET 'contact'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'contact'
       response.should be_success
     end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+  end
+
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title",
+                                  :content => " | Home")
+  end
+
+  it "should have the right title" do
+    get 'about'
+    response.should have_selector("title",
+                                  :content => " | About")
+  end
+
+
+  it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title",
+                                  :content => " | Contact")
   end
 
 end
